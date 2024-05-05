8b56966237722cfea1778661c6b6a323206934a8
QuanTranDoanAnh <td.anhquan@gmail.com>
2024-05-05 16:44:08 +0700

    Init commit

---
+++ src/Button2.jsx
@@ -0,0 +1,19 @@
+
+
+function Button2() {
+
+  let count = 0
+  const handleClick = (name) => {
+    if (count < 3) {
+      count++
+      console.log(`${name} you clicked me ${count} times`)
+    }
+    else {
+      console.log(`${name} stop clicking me`)
+    }
+  }
+
+  return (<button onClick={() => handleClick("Bro")} >Click Me!</button>)
+}
+
+export default Button2
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card() {
+  return (
+    <div className="card">
+      <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture" />
+      <h2 className="card-title">Bro Code</h2>
+      <p className="card-text">I make Youtube videos and play videog games.</p>
+    </div>
+  );
+}
+
+export default Card
